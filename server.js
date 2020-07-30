b19bc6bf0ead602587bf9466bea0a35808c73493
Xavier Marques <xaviermarques4f@gmail.com>
2020-07-30 17:48:37 +0200

    Fix bug deploy

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 //Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/checkin-bot'));
+app.use(express.static('./dist/checkin-bot'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/checkin-bot/index.html'));
+res.sendFile('index.html', {root: 'dist/checkin-bot/'});
 });
 
 // Start the app by listening on the default Heroku port
